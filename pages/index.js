d9b8563581bf6dae7cc67686db0ecb2b685639d7
Achuth Hadnoor <armorachyuth123@gmail.com>
2021-02-08 20:25:29 +0530

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,39 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import { Logo } from '../components'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Snippy | Instantly save todos,bookmarks,screenshots</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <header style={{ display: 'flex', maxWidth: '800px', margin: 'auto 0',width:'100%',alignItems:'center' }}>
+        <span style={{padding:'10px'}}><Logo /></span>
+        <span style={{flex:1}}/>
+        <nav style={{display:'flex',fontSize:'1.2em'}}>
+          <Link href="/about"><a style={{padding:'20px'}}>About</a></Link>
+          <Link href="/download"><a  style={{padding:'20px'}}>Download</a></Link>
+        </nav>
+      </header>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className={styles.main_heading}>
+          <div style={{ display: 'flex', justifyContent: 'center', margin: '2em .2em',alignItems: 'center' }}>
+            <Logo /><h1 className={styles.title}>Snippy</h1>
+          </div>
+          <h1 className={styles.title}>
+            Log your tasks to one place from anywhere
+            </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+          <p className={styles.description}>
+            Snippy allows you to <i>save and collaborate</i> on  your <b><i>tasks, bookmarks and screenshots</i></b> from anywhere on the internet
             </p>
-          </a>
+
         </div>
-      </main>
+
+       </main>
 
       <footer className={styles.footer}>
         <a
@@ -56,8 +41,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <Logo/>
         </a>
       </footer>
     </div>
