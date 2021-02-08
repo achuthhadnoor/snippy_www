5c0f44d6d1436e371d392f58f27c1b8c1645ee2f
Achuth Hadnoor <armorachyuth123@gmail.com>
2021-02-08 20:46:12 +0530

    theme 2

--- components/index.js
+++ components/index.js
@@ -1,6 +1,6 @@
 export const Logo = () => (
     <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M28.8099 15C23.5886 22.8042 15.7271 36.6566 26.0522 29.6329C36.3772 22.6091 38.2966 37.6177 37.9656 46" stroke="#F2F2F2" strokeWidth="5" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M28.8099 15C23.5886 22.8042 15.7271 36.6566 26.0522 29.6329C36.3772 22.6091 38.2966 37.6177 37.9656 46" stroke="currentColor" strokeWidth="5" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
 
 )
---
+++ components/theme.js
@@ -0,0 +1,11 @@
+const theme1 = {
+    bg: '#fee3c8',
+    secondaryBg: '#ffcc99',
+    color: '#121212'
+}
+
+const theme2 = {
+    bg: '#2640b9',
+    secondaryBg: '#314ed8',
+    color: '#F2F2F2',
+}
